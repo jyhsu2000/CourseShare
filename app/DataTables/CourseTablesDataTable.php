1dc4cfe594e96690c51353e0ae38e5f9366fd35e
KID <jyhsu2000@yahoo.com.tw>
2016-12-21 21:49:16 -0600

    Apply fixes from StyleCI (#2)
    [ci skip] [skip ci]

--- app/DataTables/CourseTablesDataTable.php
+++ app/DataTables/CourseTablesDataTable.php
@@ -3,8 +3,8 @@
 namespace App\DataTables;
 
 use App\CourseTable;
-use Illuminate\Database\Eloquent\Builder;
 use Yajra\Datatables\Services\DataTable;
+use Illuminate\Database\Eloquent\Builder;
 
 class CourseTablesDataTable extends DataTable
 {
