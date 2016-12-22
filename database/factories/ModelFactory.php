54f67c200f972ee1a6b8dbe4ce8c6611301c4951
KID <jyhsu2000@yahoo.com.tw>
2016-12-22 12:41:22 +0800

    Create UserSeeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -14,10 +14,18 @@
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
+    $isConfirmed = $faker->boolean();
+
     return [
         'name'           => $faker->name,
         'email'          => $faker->safeEmail,
         'password'       => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+        'confirm_code'   => $isConfirmed ? null : str_random(60),
+        'confirm_at'     => $isConfirmed ? $faker->dateTime : null,
+        'register_at'    => $faker->dateTime,
+        'register_ip'    => $faker->ipv4,
+        'last_login_at'  => $faker->dateTime,
+        'last_login_ip'  => $faker->ipv4,
     ];
 });
