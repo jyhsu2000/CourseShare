e289cdcf073594762e7d97029452ffe28dae283f
KID <jyhsu2000@yahoo.com.tw>
2016-12-23 10:58:06 +0800

    Update courseTable for admin

--- resources/views/admin/courseTable/index.blade.php
+++ resources/views/admin/courseTable/index.blade.php
@@ -1,53 +1,12 @@
 @extends('layouts.app')
 
-@section('title', '課表清單')
+@section('title', '課表管理')
 
 @section('content')
-    <h1>課表清單</h1>
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#createForm" data-whatever="@mdo">
-        新增課表
-    </button>
-    <div class="modal fade" id="createForm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-         aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    <h4 class="modal-title" id="exampleModalLabel">建立課表</h4>
-                </div>
-                {{ Form::open(['route' => 'courseTable.store']) }}
-                <div class="modal-body">
-                    <div class="form-group {{ $errors->has('name') ? ' has-danger' : '' }}">
-                        <label for="name" class="form-control-label">課表名稱：</label>
-                        <input id="name" type="text" name="name" value="{{ old('name') }}" required
-                               class="form-control{{ $errors->has('name') ? ' form-control-danger' : '' }}">
-                        @if ($errors->has('name'))
-                            <span class="form-control-feedback">
-                                <strong>{{ $errors->first('name') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="submit" class="btn btn-primary">建立</button>
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
-                </div>
-                {{ Form::close() }}
-            </div>
-        </div>
-    </div>
+    <h1>課表管理</h1>
     {!! $dataTable->table() !!}
 @endsection
 
 @section('js')
-    @if(session('errors'))
-        <script>
-            $(function () {
-                $('#createForm').modal({show: true});
-            });
-        </script>
-    @endif
     {!! $dataTable->scripts() !!}
 @endsection
