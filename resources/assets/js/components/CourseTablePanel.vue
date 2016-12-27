b53d8dba2eb3748525a62337305dc99fa5f58b4d
KID <jyhsu2000@yahoo.com.tw>
2016-12-28 01:22:59 +0800

    Update my courseTable

--- resources/assets/js/components/CourseTablePanel.vue
+++ resources/assets/js/components/CourseTablePanel.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="table-responsive">
         <ul class="list-group" v-sortable="{ onUpdate: onUpdate, handle: '.handle' }">
-            <li class="list-group-item"  v-for="course_table in course_tables">
+            <li class="list-group-item" v-for="course_table in course_tables">
                 <i class="fa fa-arrows-v btn btn-secondary handle" aria-hidden="true"></i>
                 <a href="{{ api }}/{{ course_table.id }}">
+                    <i class="fa fa-globe fa-fw" aria-hidden="true" title="公開" v-if="course_table.public"></i>
+                    <i class="fa fa-lock fa-fw" aria-hidden="true" title="私人" v-else=""></i>
                     {{ course_table.name }}
                 </a>
             </li>
